a5252c4a94a9b5d89ae616c7dd476b09955e5166
hatsumikana <kana.nanomi@gmail.com>
2020-11-18 11:05:08 +0800

    first commit

---
+++ app/src/main/java/com/hatsumi/bluentry_declaration/ui/declaration/DeclarationFragment.java
@@ -0,0 +1,35 @@
+package com.hatsumi.bluentry_declaration.ui.declaration;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.hatsumi.bluentry_declaration.R;
+
+public class DeclarationFragment extends Fragment {
+
+    private DeclarationViewModel declarationViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        declarationViewModel =
+                ViewModelProviders.of(this).get(DeclarationViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_declaration, container, false);
+        final TextView textView = root.findViewById(R.id.text_declaration);
+        declarationViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
---
+++ app/src/main/java/com/hatsumi/bluentry_declaration/ui/declaration/DeclarationViewModel.java
@@ -0,0 +1,19 @@
+package com.hatsumi.bluentry_declaration.ui.declaration;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DeclarationViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public DeclarationViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is notifications fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
