9b1ed764d7e23ee0f96ff6029c551ad3baa60c29
invertedplant <73229862+invertedplant@users.noreply.github.com>
2021-04-23 10:07:40 +0800

    Update tests
    Added tests

--- app/src/test/java/com/cabdab/wifi/CoordManagerTest.java
+++ app/src/test/java/com/cabdab/wifi/CoordManagerTest.java
@@ -5,6 +5,7 @@ import android.graphics.PointF;
 import com.cabdab.wifi.ui.mapview.CoordManager;
 
 import org.junit.Test;
+import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
 
@@ -22,9 +23,32 @@ public class CoordManagerTest {
     @Test
     public void testCoordManager() {
         // Initialize CoordManager
-       CoordManager coordManager = new CoordManager(100, 100, 50, 50);
+        CoordManager coordManager = new CoordManager(100, 100, 50, 50);
         PointF testPoint = new PointF(1.0f, 1.0f);
         coordManager.moveBySingleTap(testPoint);
         System.out.println(testPoint);
+    }
+
+    @Test
+    public void monkeyCoordManager(){
+        // Initialize CoordManager
+        CoordManager coordManager = new CoordManager(100, 100, 50, 50);
+        Random random = new Random();
+        // keep tapping randomly lots of times, not really a monkey test but you don't want to crash the app
+        for (int i = 0; i <= 128; i++) {
+            float random1 = random.nextFloat() * (random.nextBoolean() ? -1 : 1);
+            float random2 = random.nextFloat() * (random.nextBoolean() ? -1 : 1);
+            PointF testPoint = new PointF(random1, random2);
+            coordManager.moveBySingleTap(testPoint);
+            System.out.println(testPoint);
+            /* you might need this if it taps TOO fast
+            try{
+                Thread.sleep(100);
+            }
+            catch (InterruptedException ex){
+                continue;
+            }
+             */
+        }
     }
 }
