19f1eb82ff8f85b22e8bee70b9df94773657b9fe
Lai Yan Hong <73103066+Icychiller@users.noreply.github.com>
2021-04-23 21:00:35 +0800

    Update DataSet.java

--- app/src/main/java/com/cabdab/wifi/ui/DataSet.java
+++ app/src/main/java/com/cabdab/wifi/ui/DataSet.java
@@ -9,8 +9,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import javax.sound.midi.SysexMessage;
-
 public class DataSet {
     private HashSet<String> allMACAddr = new HashSet<>();
     private ArrayList<RunData> mapData = new ArrayList<>();
